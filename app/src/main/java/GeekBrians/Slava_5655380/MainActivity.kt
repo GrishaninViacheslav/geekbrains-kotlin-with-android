72c4ca41b5e618602d5c51c896148def8d273b8b
GrishaninVyacheslav <64549372+GrishaninVyacheslav@users.noreply.github.com>
2021-04-28 14:47:48 +0300

    Создание Android Studio проекта

---
+++ app/src/main/java/GeekBrians/Slava_5655380/MainActivity.kt
@@ -0,0 +1,18 @@
+package GeekBrians.Slava_5655380
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import GeekBrians.Slava_5655380.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/GeekBrians/Slava_5655380/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package GeekBrians.Slava_5655380.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
