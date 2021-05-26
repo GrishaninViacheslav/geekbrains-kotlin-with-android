e453463c0b7e8d07915518d601acdb78636b03d0
GrishaninVyacheslav <64549372+GrishaninVyacheslav@users.noreply.github.com>
2021-05-26 18:32:01 +0300

    example

--- app/src/main/java/GeekBrians/Slava_5655380/ui/view/MainActivity.kt
+++ app/src/main/java/GeekBrians/Slava_5655380/ui/view/MainActivity.kt
@@ -1,13 +1,18 @@
 package GeekBrians.Slava_5655380.ui.view
 
 import GeekBrians.Slava_5655380.R
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
+    companion object {
+        var PACKAGE_NAME: String? = null
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        PACKAGE_NAME = packageName
         setContentView(R.layout.main_activity)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
--- app/src/main/java/GeekBrians/Slava_5655380/ui/viewmodel/RecommendationFeed/Adapter.kt
+++ app/src/main/java/GeekBrians/Slava_5655380/ui/viewmodel/RecommendationFeed/Adapter.kt
@@ -1,15 +1,14 @@
 package GeekBrians.Slava_5655380.ui.viewmodel.RecommendationFeed
 
 import GeekBrians.Slava_5655380.R
+import GeekBrians.Slava_5655380.ui.view.MainActivity
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.View.GONE
 import android.view.View.VISIBLE
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.ProgressBar
-import android.widget.Space
-import android.widget.TextView
+import android.widget.*
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 
@@ -23,15 +22,23 @@ class Adapter(private val viewModel: RecommendationFeedViewModel) : RecyclerView
     override fun onBindViewHolder(viewHolder: ViewHolder, i: Int) {
         when(val rvItemState = viewModel.getItem(i)){
             is RVItemState.Success -> {
-                viewHolder.view.findViewById<Space>(R.id.background_video).visibility = VISIBLE
+                viewHolder.view.findViewById<VideoView>(R.id.background_video).visibility = VISIBLE
                 viewHolder.view.findViewById<ImageView>(R.id.movie_poster).visibility = VISIBLE
                 viewHolder.view.findViewById<TextView>(R.id.localized_title).visibility = VISIBLE
                 viewHolder.view.findViewById<ProgressBar>(R.id.progress_bar).visibility = GONE
 
+                var videoView = viewHolder.view.findViewById(R.id.background_video) as VideoView
+                videoView.setVideoURI(Uri.parse("android.resource://" +  MainActivity.PACKAGE_NAME + "/" + R.raw.test))
+                videoView.setOnPreparedListener { mp ->
+                    mp.setVolume(0f, 0f)
+                    mp.isLooping = true
+                }
+                videoView.start()
+
                 viewHolder.view.findViewById<TextView>(R.id.localized_title).text = rvItemState.movieMetadata.index.toString()
             }
             is RVItemState.Loading -> {
-                viewHolder.view.findViewById<Space>(R.id.background_video).visibility = GONE
+                viewHolder.view.findViewById<VideoView>(R.id.background_video).visibility = GONE
                 viewHolder.view.findViewById<ImageView>(R.id.movie_poster).visibility = GONE
                 viewHolder.view.findViewById<TextView>(R.id.localized_title).visibility = GONE
                 viewHolder.view.findViewById<ProgressBar>(R.id.progress_bar).visibility = VISIBLE
