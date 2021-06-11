515973ba1bfd1166ef88195f469785a0afc4f1ea
GrishaninVyacheslav <64549372+GrishaninVyacheslav@users.noreply.github.com>
2021-06-12 00:46:56 +0300

    Решение задания 2 урока 7

--- app/src/main/java/GeekBrians/Slava_5655380/ui/viewmodel/recommendationfeed/RVItemState.kt
+++ app/src/main/java/GeekBrians/Slava_5655380/ui/viewmodel/recommendationfeed/RVItemState.kt
@@ -7,9 +7,14 @@ import android.os.Looper
 import com.google.android.exoplayer2.MediaItem
 import com.google.android.exoplayer2.Player
 import com.google.android.exoplayer2.SimpleExoPlayer
+import com.squareup.picasso.Picasso
 
 sealed class RVItemState {
     class Success(movieMetadata: MovieMetadata) : RVItemState() {
+        companion object {
+            val imgBaseUrl: String = "https://image.tmdb.org/t/p/w500/"
+        }
+
         val movieDataItem: MovieDataItem = MovieDataItem(
             index = movieMetadata.index,
             localizedTitle = movieMetadata.originalTitle,
@@ -17,6 +22,7 @@ sealed class RVItemState {
             genres = arrayOf("sci-fi", "horror"),
             director = "Режисёр Режисёрович",
             screenwriter = "Сценарист Сценаристович",
+            poster = if (movieMetadata.posterUri != null) Picasso.get().load("$imgBaseUrl${movieMetadata.posterUri}") else null,
             // TODO: сделать что-то вроде пула плееров размер которого был бы равен feedBuffer.size,
             //             чтобы плееры можно было переиспользовать назначая им новый MediaItem,
             //             вместо создания новго плеера
