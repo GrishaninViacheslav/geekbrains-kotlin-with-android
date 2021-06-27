7b4063edef56f6d9436f82db141102107a0c0dfb
GrishaninVyacheslav <64549372+GrishaninVyacheslav@users.noreply.github.com>
2021-06-27 15:21:52 +0300

    Добавление экрана со списком контактов

--- app/src/main/java/GeekBrians/Slava_5655380/ui/view/CollectionsFeedFragment.kt
+++ app/src/main/java/GeekBrians/Slava_5655380/ui/view/CollectionsFeedFragment.kt
@@ -1,33 +1,158 @@
 package GeekBrians.Slava_5655380.ui.view
 
-import androidx.lifecycle.ViewModelProvider
+import GeekBrians.Slava_5655380.R
+import GeekBrians.Slava_5655380.databinding.CollectionsFeedFragmentBinding
+import android.Manifest
+import android.app.AlertDialog
+import android.content.ContentResolver
+import android.content.Context
+import android.content.pm.PackageManager
+import android.database.Cursor
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import GeekBrians.Slava_5655380.R
-import GeekBrians.Slava_5655380.ui.viewmodel.collectionsfeed.CollectionsFeedViewModel
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.core.content.ContextCompat
 
 class CollectionsFeedFragment : Fragment() {
+    private var _binding: CollectionsFeedFragmentBinding? = null
+    private val binding get() = _binding!!
 
-    companion object {
-        fun newInstance() = CollectionsFeedFragment()
+    val REQUEST_CODE = 42
+
+    // Проверяем, разрешено ли чтение контактов
+    private fun checkPermission() {
+        context?.let {
+            when {
+                ContextCompat.checkSelfPermission(
+                    it,
+                    Manifest.permission.READ_CONTACTS
+                ) ==
+                        PackageManager.PERMISSION_GRANTED -> {
+//Доступ к контактам на телефоне есть
+                    getContacts()
+                }
+//Опционально: если нужно пояснение перед запросом разрешений
+                shouldShowRequestPermissionRationale(Manifest.permission.READ_CONTACTS) -> {
+                    AlertDialog.Builder(it)
+                        .setTitle("Доступ к контактам")
+                        .setMessage("Объяснение")
+                        .setPositiveButton("Предоставить доступ") { _, _ ->
+                            requestPermission()
+                        }
+                        .setNegativeButton("Не надо") { dialog, _ ->
+                            dialog.dismiss()
+                        }
+                        .create()
+                        .show()
+                }
+                else -> {
+//Запрашиваем разрешение
+                    requestPermission()
+                }
+            }
+        }
     }
 
-    private lateinit var viewModel: CollectionsFeedViewModel
+    private fun requestPermission() {
+        requestPermissions(arrayOf(Manifest.permission.READ_CONTACTS), REQUEST_CODE)
+    }
+
+    private fun getContacts() {
+        context?.let {
+// Получаем ContentResolver у контекста
+            val contentResolver: ContentResolver = it.contentResolver
+// Отправляем запрос на получение контактов и получаем ответ в виде
+            // Cursor'а
+            val cursorWithContacts: Cursor? = contentResolver.query(
+                ContactsContract.Contacts.CONTENT_URI,
+                null,
+                null,
+                null,
+                ContactsContract.Contacts.DISPLAY_NAME + " ASC"
+            )
+            cursorWithContacts?.let { cursor ->
+                for (i in 0..cursor.count) {
+// Переходим на позицию в Cursor'е
+                    if (cursor.moveToPosition(i)) {
+// Берём из Cursor'а столбец с именем
+                        val name =
+                        cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME))
+                        Log.d("[MYLOG]", "name: $name")
+                        addView(it, name)
+                    }
+                }
+            }
+            cursorWithContacts?.close()
+        }
+    }
+
+    private fun addView(context: Context, textToShow: String) {
+        binding.containerForContacts.addView(AppCompatTextView(context).apply {
+            text = textToShow
+            textSize = resources.getDimension(R.dimen.text_size)
+        })
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() =
+            CollectionsFeedFragment()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.collections_feed_fragment, container, false)
+        _binding = CollectionsFeedFragmentBinding.inflate(
+            inflater, container,
+            false
+        )
+        return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(CollectionsFeedViewModel::class.java)
-        // TODO: Use the ViewModel
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        checkPermission()
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    // Обратный вызов после получения разрешений от пользователя
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>, grantResults: IntArray
+    ) {
+        when (requestCode) {
+            REQUEST_CODE -> {
+// Проверяем, дано ли пользователем разрешение по нашему запросу
+                if ((grantResults.isNotEmpty() &&
+                            grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                ) {
+                    getContacts()
+                } else {
+// Поясните пользователю, что экран останется пустым, потому
+                    // что доступ к контактам не предоставлен
+                    context?.let {
+                        AlertDialog.Builder(it)
+                            .setTitle("Доступ к контактам")
+                            .setMessage("Объяснение")
+                            .setNegativeButton("Закрыть") { dialog, _ ->
+                                dialog.dismiss()
+                            }
+                            .create()
+                            .show()
+                    }
+                }
+                return
+            }
+        }
+    }
 }
