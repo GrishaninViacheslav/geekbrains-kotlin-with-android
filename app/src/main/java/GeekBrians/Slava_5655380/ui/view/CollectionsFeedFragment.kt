ccc28599360463edfd6498624b27da2ecaa38417
GrishaninVyacheslav <64549372+GrishaninVyacheslav@users.noreply.github.com>
2021-06-30 15:22:17 +0300

    Добавление возможности позвонить по номеру из списка контактов

--- app/src/main/java/GeekBrians/Slava_5655380/ui/view/CollectionsFeedFragment.kt
+++ app/src/main/java/GeekBrians/Slava_5655380/ui/view/CollectionsFeedFragment.kt
@@ -6,52 +6,59 @@ import android.Manifest
 import android.app.AlertDialog
 import android.content.ContentResolver
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.database.Cursor
+import android.net.Uri
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.provider.ContactsContract
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import android.view.ViewGroup
 import androidx.appcompat.widget.AppCompatTextView
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+
 
 class CollectionsFeedFragment : Fragment() {
+    private val REQUEST_CODE = 42
     private var _binding: CollectionsFeedFragmentBinding? = null
     private val binding get() = _binding!!
+    private val handler = Handler(Looper.getMainLooper())
 
-    val REQUEST_CODE = 42
-
-    // Проверяем, разрешено ли чтение контактов
     private fun checkPermission() {
         context?.let {
             when {
-                ContextCompat.checkSelfPermission(
-                    it,
-                    Manifest.permission.READ_CONTACTS
-                ) ==
-                        PackageManager.PERMISSION_GRANTED -> {
-//Доступ к контактам на телефоне есть
+                ContextCompat.checkSelfPermission(it, Manifest.permission.READ_CONTACTS)
+                        == PackageManager.PERMISSION_GRANTED
+                        &&
+                        ContextCompat.checkSelfPermission(it, Manifest.permission.CALL_PHONE)
+                        == PackageManager.PERMISSION_GRANTED
+                -> {
                     getContacts()
                 }
-//Опционально: если нужно пояснение перед запросом разрешений
-                shouldShowRequestPermissionRationale(Manifest.permission.READ_CONTACTS) -> {
+                shouldShowRequestPermissionRationale(Manifest.permission.READ_CONTACTS)
+                        ||
+                        shouldShowRequestPermissionRationale(Manifest.permission.CALL_PHONE)
+                -> {
                     AlertDialog.Builder(it)
                         .setTitle("Доступ к контактам")
-                        .setMessage("Объяснение")
-                        .setPositiveButton("Предоставить доступ") { _, _ ->
+                        .setMessage("Для работы данного меню приложению необходимы разрешения на чтения контактов и совершение вызовов")
+                        .setPositiveButton("Дать разрешения") { _, _ ->
                             requestPermission()
                         }
-                        .setNegativeButton("Не надо") { dialog, _ ->
+                        .setNegativeButton("Отказать в разрешениях") { dialog, _ ->
                             dialog.dismiss()
                         }
                         .create()
                         .show()
                 }
                 else -> {
-//Запрашиваем разрешение
                     requestPermission()
                 }
             }
@@ -59,43 +66,70 @@ class CollectionsFeedFragment : Fragment() {
     }
 
     private fun requestPermission() {
-        requestPermissions(arrayOf(Manifest.permission.READ_CONTACTS), REQUEST_CODE)
+        requestPermissions(
+            arrayOf(
+                Manifest.permission.READ_CONTACTS,
+                Manifest.permission.CALL_PHONE
+            ), REQUEST_CODE
+        )
     }
 
     private fun getContacts() {
         context?.let {
-// Получаем ContentResolver у контекста
-            val contentResolver: ContentResolver = it.contentResolver
-// Отправляем запрос на получение контактов и получаем ответ в виде
-            // Cursor'а
-            val cursorWithContacts: Cursor? = contentResolver.query(
-                ContactsContract.Contacts.CONTENT_URI,
-                null,
-                null,
-                null,
-                ContactsContract.Contacts.DISPLAY_NAME + " ASC"
-            )
-            cursorWithContacts?.let { cursor ->
-                for (i in 0..cursor.count) {
-// Переходим на позицию в Cursor'е
-                    if (cursor.moveToPosition(i)) {
-// Берём из Cursor'а столбец с именем
-                        val name =
-                        cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME))
-                        Log.d("[MYLOG]", "name: $name")
-                        addView(it, name)
+            Thread {
+                val contentResolver: ContentResolver = it.contentResolver
+                val cursorWithContacts: Cursor = contentResolver.query(
+                    ContactsContract.Contacts.CONTENT_URI,
+                    null,
+                    null,
+                    null,
+                    ContactsContract.Contacts.DISPLAY_NAME + " ASC"
+                ) ?: return@Thread
+                cursorWithContacts.moveToFirst()
+                do {
+                    val name: String = cursorWithContacts.getString(
+                        cursorWithContacts.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME)
+                    )
+                    val id: String = cursorWithContacts.getString(
+                        cursorWithContacts.getColumnIndex(ContactsContract.Contacts.NAME_RAW_CONTACT_ID)
+                    )
+                    val phones: Cursor = contentResolver
+                        .query(
+                            ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                            null,
+                            ContactsContract.CommonDataKinds.Phone.RAW_CONTACT_ID + " = " + id,
+                            null,
+                            null
+                        )!!
+                    if (phones != null) {
+                        while (phones.moveToNext()) {
+                            val phoneNumber = phones.getString(
+                                phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER)
+                            )
+                            Log.d("[MYLOG]", "addView: $name $phoneNumber")
+                            addView(it, name, phoneNumber)
+                        }
+                        phones.close()
                     }
-                }
-            }
-            cursorWithContacts?.close()
+                } while (cursorWithContacts.moveToNext())
+                cursorWithContacts.close()
+            }.start()
         }
     }
 
-    private fun addView(context: Context, textToShow: String) {
-        binding.containerForContacts.addView(AppCompatTextView(context).apply {
-            text = textToShow
-            textSize = resources.getDimension(R.dimen.text_size)
-        })
+    private fun addView(context: Context, name: String, phoneNumber: String) {
+        val textView = AppCompatTextView(context).apply {
+            text = "$name\n$phoneNumber"
+            textSize = resources.getDimension(R.dimen.text_small_size)
+            setOnClickListener {
+                val callIntent = Intent(Intent.ACTION_CALL)
+                callIntent.data = Uri.parse("tel:" + phoneNumber) //change the number
+                startActivity(callIntent)
+            }
+        }
+        handler.post {
+            binding.containerForContacts.addView(textView)
+        }
     }
 
     companion object {
@@ -125,31 +159,25 @@ class CollectionsFeedFragment : Fragment() {
         _binding = null
     }
 
-    // Обратный вызов после получения разрешений от пользователя
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<String>, grantResults: IntArray
     ) {
         when (requestCode) {
             REQUEST_CODE -> {
-// Проверяем, дано ли пользователем разрешение по нашему запросу
                 if ((grantResults.isNotEmpty() &&
-                            grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                            grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED)
                 ) {
                     getContacts()
                 } else {
-// Поясните пользователю, что экран останется пустым, потому
-                    // что доступ к контактам не предоставлен
-                    context?.let {
-                        AlertDialog.Builder(it)
-                            .setTitle("Доступ к контактам")
-                            .setMessage("Объяснение")
-                            .setNegativeButton("Закрыть") { dialog, _ ->
-                                dialog.dismiss()
-                            }
-                            .create()
-                            .show()
-                    }
+                    AlertDialog.Builder(context)
+                        .setTitle("Доступ к контактам")
+                        .setMessage("Данное меню не функционирует так как отсутствует разрешение на чтение контактов и своершения вызовов")
+                        .setNegativeButton("Закрыть") { dialog, _ ->
+                            dialog.dismiss()
+                        }
+                        .create()
+                        .show()
                 }
                 return
             }
